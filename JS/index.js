0ed89f91897cc1955d17dcfd9bd4185650e1f5e1
Wynterwolf <shinyguns@gmail.com>
2021-01-12 16:26:04 -0600

    add form, check data

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,16 @@
-fetch('http://localhost:3000/users/') 
- .then((response) => {
-    return response.json();
+// fetch('http://localhost:3000/users/') 
+//  .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+
+window.onload = function () {
+  let form = document.getElementById('sheet');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(sheet.elements)
   })
-  .then((data) => {
-    console.log(data);
-  })
+};
